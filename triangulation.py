112df43de8e06ffd1b013726c9157b76da703eca
patatahooligan <filwn.oikonomou@gmail.com>
2013-04-09 21:36:27 +0300

    Started convex hull algorithm

--- triangulation.py
+++ triangulation.py
@@ -1,6 +1,8 @@
 from random import randint, seed
+from copy import copy
 import Image
 import ImageDraw
+
 
 class Point(object):
     """ A single point on the plane
@@ -17,8 +19,9 @@ class Point(object):
 
         (self.x, self.y) = (x, y)
 
-    def get_coordinates(self):
-        """ Return a tuple of the points x,y.
+    @property
+    def coordinates(self):
+        """ Return a tuple of the point's x,y.
         """
 
         return (self.x, self.y)
@@ -40,7 +43,8 @@ class Line(object):
         self.start = start
         self.end = end
 
-    def get_points(self):
+    @property
+    def points(self):
         """ Returns a tuple of the start and end coordinates in the form of
         ((startx,starty),(endx,endy))
         """
@@ -64,14 +68,14 @@ class Graph(object):
         """
 
         for point in points:
-            self.imd.point((point.get_coordinates()), fill="black")
+            self.imd.point((point.coordinates), fill="black")
 
     def paint_lines (self, lines):
         """ Draws all the given lines.
         """
 
         for line in lines:
-            (start,end) = line.get_points()
+            (start,end) = line.points
             self.imd.line([start,end], fill="black")
 
     def show(self):
@@ -95,7 +99,7 @@ class Plane(object):
         self.lines = []
 
     def create_points(self, no_of_points):
-        """ Creates given number of points as xy coordinates.
+        """ Creates given number of points with random coordinates.
         """
 
         points = []
@@ -108,7 +112,60 @@ class Plane(object):
         """ Connects two points on the plane.
         """
 
-        self.lines.append(Line(point1.get_coordinates(), point2.get_coordinates()))
+        self.lines.append(Line(point1.coordinates, point2.coordinates))
+
+    def compute_convex_hull(self):
+        """ Compute the convex hull of all the points on the plane
+        """
+
+        # Sort all points by their x coordinate
+        self.points.sort(key=lambda x:x.coordinates[0])
+
+        # Start calculating the upper and lower parts of the hull from
+        # the rightmost and leftmost points respectively
+        upper_hull = [self.points[0]]
+        lower_hull = [self.points[-1]]
+
+        for i in range(1, len(self.points)):
+            if len(upper_hull) > 1:
+                (x,y) = self.points[i].coordinates
+                (prevx, prevy) = upper_hull[-1].coordinates
+                (Ox, Oy) = upper_hull[-2].coordinates
+
+                while (prevy-Oy)*(x-Ox) >= (y-Oy)*(prevx-Ox):
+                    upper_hull.pop()
+                    if len(upper_hull)<2:
+                        break
+                    else:
+                        (prevx, prevy) = upper_hull[-1].coordinates
+                        (Ox, Oy) = upper_hull[-2].coordinates
+            upper_hull.append(self.points[i])
+
+            if len(lower_hull) > 1:
+                (x,y) = self.points[i].coordinates
+                (prevx, prevy) = lower_hull[-1].coordinates
+                (Ox, Oy) = lower_hull[-2].coordinates
+
+                while (prevy-Oy)*(x-Ox) >= (y-Oy)*(prevx-Ox):
+                    lower_hull.pop()
+                    if len(lower_hull)<2:
+                        break
+                    else:
+                        (prevx, prevy) = lower_hull[-1].coordinates
+                        (Ox, Oy) = lower_hull[-2].coordinates
+            lower_hull.append(self.points[i])
+
+            # TODO : add lower_hull part
+
+        for i in range (1, len(upper_hull)):
+            start = upper_hull[i-1].coordinates
+            end = upper_hull[i].coordinates
+            self.lines.append(Line(start,end))
+
+        for i in range (1, len(lower_hull)):
+            start = lower_hull[i-1].coordinates
+            end = lower_hull[i].coordinates
+            self.lines.append(Line(start,end))
 
     def show_points(self):
         """ Creates and displays an image with all the points of the plane.
